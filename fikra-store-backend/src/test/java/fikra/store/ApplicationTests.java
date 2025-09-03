8e1b1d13da8a46d1e79f6ef12bbf4bb579ac08f9
ahmadabdelbary2001 <ahmadabdelbary2001@gmail.com>
2025-09-04 01:29:50 +0300

    Initiate an empty SpringBoot project for fikra-store-backend

---
+++ fikra-store-backend/src/test/java/fikra/store/ApplicationTests.java
@@ -0,0 +1,13 @@
+package fikra.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
